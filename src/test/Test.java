904a870459327fd18a779fb22fa36fdc95518c51
raku <raku@gmail>
2017-01-05 23:52:21 +0900

    commit 2017/1/5

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,15 @@
 package test;
 
 public class Test {
+    //程序的主函数入门
+
+    public static void main(String args[])
+
+    {
+         //输出打印字符语句
+
+        System.out.println("HelloWorld！");
+
+    }
 
 }
