01b4bafcea0d27963d76e89f9042ca24fd0052ca
raku <raku@gmail>
2017-01-07 13:19:28 +0900

    test 0107_001 commit by Raku 0107 Update Test.java
    updated by Raku 1946
    
    ★★★

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,8 +6,8 @@ public class Test {
     public static void main(String args[])
 
     {
-         //输出打印字符语句222333444
-
+         //输出打印字符语句222333444 aaa１９４６
+    	 //updated by Raku 001
         System.out.println("HelloWorld！");
 
     }
