069bae4599427e2a20ec608fe610d1ddf2733e9e
raku <raku@gmail>
2017-02-10 23:08:03 +0900

    commit 20170210

--- src/test/Test.java
+++ src/test/Test.java
@@ -17,7 +17,7 @@ public class Test {
     	//updated by Raku 002
     	//updated by Raku 003
     	//updated by Raku 004
-    	//updated by Raku 005
+    	//updated by Raku 005567
         System.out.println("HelloWorld！");
 
     }
